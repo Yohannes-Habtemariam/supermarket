d013c7a557b44d03da4597561581dc7681c43e74
Yohannes <uelandrae@gmail.com>
2022-10-06 16:15:07 +0200

    header

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import { NavLink } from "react-router-dom";
 import supermarketLogo from "../../assents/lisaSupermarketLogo.png";
-import shoppingCart from "../../assents/shopping-cart.png"
+import shoppingCart from "../../assents/cart.png"
 import "./Navbar.scss";
 
 const Navbar = () => {
   return (
     <header className='header-container'>
-        <nav className='navigation-bar'>
+        <nav className='horizontal-navigation-bar'>
 
             <div className='language-registter-login'>
-                <h4>SHOP TODAY AND SAVE AT LEAST 20% DISCOUNT</h4>
+                <h5>SHOP TODAY AND SAVE AT LEAST 20% DISCOUNT</h5>
                 <div>
                     <NavLink to="/register"> Register </NavLink>
                     <NavLink to="/register"> Login </NavLink>
@@ -32,34 +32,37 @@ const Navbar = () => {
                     <input type="search" name="" id="" />
                 </div>
 
-                <figure>
+                <figure className='shopping-cart'>
                     <img src={shoppingCart} alt="Shopping Cart" />
                 </figure>
 
             </div>
                 
-            <ul className='nav-menu'>
-                <li className="nav-item">
-                    <NavLink to="/home" style={({ isActive }) => ({ textDecoration: isActive && "underline", color: isActive && "red"})}> Home </NavLink>
-                </li>
+            <div className='text-logo-horizontal-nav-menue'>
+                <h3> <NavLink to="/home">Lisa Supermarket</NavLink></h3>
+                <ul className='horizontal-nav-menu'>
+                    <li className="horizontal-nav-item">
+                        <NavLink to="/home" style={({ isActive }) => ({ textDecoration: isActive && "underline", color: isActive && "red"})}> Home </NavLink>
+                    </li>
 
-                <li className="nav-item">
-                    <NavLink to="/about" style={({ isActive }) => ({ textDecoration: isActive && "underline", color: isActive && "red"})}> About </NavLink>
-                </li>
+                    <li className="horizontal-nav-item">
+                        <NavLink to="/about" style={({ isActive }) => ({ textDecoration: isActive && "underline", color: isActive && "red"})}> About </NavLink>
+                    </li>
 
-                <li className="nav-item">
-                    <NavLink to="/service" style={({ isActive }) => ({ textDecoration: isActive && "underline", color: isActive && "red"})}> Services </NavLink>
-                </li>
+                    <li className="horizontal-nav-item">
+                        <NavLink to="/service" style={({ isActive }) => ({ textDecoration: isActive && "underline", color: isActive && "red"})}> Services </NavLink>
+                    </li>
 
-                <li className="nav-item">
-                    <NavLink to="/research" style={({ isActive }) => ({ textDecoration: isActive && "underline", color: isActive && "red"})}> Research </NavLink>
-                </li>
+                    <li className="horizontal-nav-item">
+                        <NavLink to="/research" style={({ isActive }) => ({ textDecoration: isActive && "underline", color: isActive && "red"})}> Research </NavLink>
+                    </li>
 
-                <li className="nav-item">
-                    <NavLink to="/contact" style={({ isActive }) => ({ textDecoration: isActive && "underline", color: isActive && "red"})}> Contact </NavLink>
-                </li>
+                    <li className="horizontal-nav-item">
+                        <NavLink to="/contact" style={({ isActive }) => ({ textDecoration: isActive && "underline", color: isActive && "red"})}> Contact </NavLink>
+                    </li>
                 
             </ul>
+            </div>
         </nav>
     </header>
   )
--- frontend/src/views/homePage/Home.jsx
+++ frontend/src/views/homePage/Home.jsx
@@ -1,8 +1,52 @@
-import React from 'react'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import "./Home.scss";
+import fishMeal1 from "../../assents/ready-fish-1.jpg";
+import fishMeal2 from "../../assents/ready-fish-2.jpg";
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <main>
+      <div className='left-navigation-images-container'>
+        <nav className='vertical-navigation-bar'>
+            <h1>Categories</h1>
+            <u className='vertical-nav-menu'>
+                <li className='vertical-nav-item'> <NavLink to="/organicMeal"> Organic Meal </NavLink></li>
+
+                <li className='vertical-nav-item'> <NavLink to="/breads"> Breads & Bakery </NavLink></li>
+
+                <li className='vertical-nav-item'> <NavLink to="/dairy"> Dairy, Cheese & Eggs Fresh Flower</NavLink></li>
+
+                <li className='vertical-nav-item'> <NavLink to="/flowers"> Fresh Flowers</NavLink></li>
+
+                <li className='vertical-nav-item'> <NavLink to="/meat"> Meat & Seafood </NavLink></li>
+
+                <li className='vertical-nav-item'> <NavLink to="/beverages"> Beverages & Alcoholic beverage </NavLink></li>
+
+                <li className='vertical-nav-item'> <NavLink to="/chocolate"> Chocolate & Candy </NavLink></li>
+
+                <li className='vertical-nav-item'> <NavLink to="/breakfast"> Breakfast Foods </NavLink></li>
+
+                <li className='vertical-nav-item'> <NavLink to="/lunch"> Lunch & Dinner Foods </NavLink></li>
+
+                <li className='vertical-nav-item'> <NavLink to="/clothes"> Clothes </NavLink></li>
+
+                <li className='vertical-nav-item'> <NavLink to="/shoes"> Shoes </NavLink></li>
+            </u>
+        </nav>
+        <figure>
+          <img src={fishMeal1} alt=" Fish Meal" />
+        </figure>
+      </div>
+
+      <section>
+        <h2> Featured Collection</h2>
+        <figure>
+          <img src="" alt="" />
+        </figure>
+      </section>
+
+    </main>
   )
 }
 
