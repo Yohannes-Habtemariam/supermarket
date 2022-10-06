aac4aa03afe63229ed78117dd80f2fdd892199ee
Yohannes <uelandrae@gmail.com>
2022-10-06 11:59:06 +0200

    header

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,16 +1,43 @@
 import React from 'react';
 import { NavLink } from "react-router-dom";
-import supermarketLogo from "../../assents/lisa-supermarket.png";
+import supermarketLogo from "../../assents/lisaSupermarketLogo.png";
+import shoppingCart from "../../assents/shopping-cart.png"
+import "./Navbar.scss";
 
 const Navbar = () => {
   return (
     <header className='header-container'>
         <nav className='navigation-bar'>
 
-            <figure className='logo-container'>
-                <img src={supermarketLogo} alt="Supermarket Logo" />
-            </figure>
+            <div className='language-registter-login'>
+                <h4>SHOP TODAY AND SAVE AT LEAST 20% DISCOUNT</h4>
+                <div>
+                    <NavLink to="/register"> Register </NavLink>
+                    <NavLink to="/register"> Login </NavLink>
+                    <select name="language">
+                        <option value="defaul">Select Language</option>
+                        <option value="english">English</option>
+                        <option value="deutsch">German</option>
+                        <option value="tigrigna">Tigrigna</option>
+                    </select>
+                </div>
+            </div>
 
+            <div className='logo-search-cart'>
+                <figure className='logo-container'>
+                    <img src={supermarketLogo} alt="Supermarket Logo" />
+                </figure>
+
+                <div>
+                    <input type="search" name="" id="" />
+                </div>
+
+                <figure>
+                    <img src={shoppingCart} alt="Shopping Cart" />
+                </figure>
+
+            </div>
+                
             <ul className='nav-menu'>
                 <li className="nav-item">
                     <NavLink to="/home" style={({ isActive }) => ({ textDecoration: isActive && "underline", color: isActive && "red"})}> Home </NavLink>
@@ -21,7 +48,7 @@ const Navbar = () => {
                 </li>
 
                 <li className="nav-item">
-                    <NavLink to="/service" style={({ isActive }) => ({ textDecoration: isActive && "underline", color: isActive && "red"})}> Service </NavLink>
+                    <NavLink to="/service" style={({ isActive }) => ({ textDecoration: isActive && "underline", color: isActive && "red"})}> Services </NavLink>
                 </li>
 
                 <li className="nav-item">
